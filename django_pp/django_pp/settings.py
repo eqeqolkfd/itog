a403242e964f85b3335c397c64e86d9b0e2fd673
Emil <isip_e.t.hodjakhov@mpt.ru>
2025-05-09 23:17:44 +0300

    Итоговый проект

--- django_pp/django_pp/settings.py
+++ django_pp/django_pp/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-6o^y$qb(g03(@5v!m#zliv_(l1uy2ld-39pdjrsd4p)1$z7g-t
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['firstproject.pythonanywhere.com']
 
 
 # Application definition
@@ -125,6 +125,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
